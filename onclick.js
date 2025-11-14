e743efef369c6d659ce4cea7dde0ac72b03a50ef
Aivaras_Rabcevicius <aivarabcas@gmail.com>
2025-11-14 23:24:14 +0200

    small tweak lole

--- onclick.js
+++ onclick.js
@@ -1,7 +1,7 @@
-document.querySelectorAll('img').forEach(i => {
+document.querySelectorAll('figure').forEach(i => {
     i.addEventListener('click', evt => {
         if (i.classList.contains('zoomed'))
-            i.style.width = '300px'
+            i.style.width = ''
         else {
             i.style.width = "100%"
         }
